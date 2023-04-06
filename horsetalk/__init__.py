603e4f6419bde20231b923716992a359b850de7a
peaky76 <robertjamespeacock@gmail.com>
2023-04-06 23:25:18 +0100

    feat: Obstacle enum

--- horsetalk/__init__.py
+++ horsetalk/__init__.py
@@ -1,6 +1,7 @@
 from .breed import Breed
 from .coat_colour import CoatColour
 from .gender import Gender
+from .obstacle import Obstacle
 from .sex import Sex
 
-__all__ = ["Breed", "CoatColour", "Gender", "Sex"]
+__all__ = ["Breed", "CoatColour", "Gender", "Obstacle", "Sex"]
