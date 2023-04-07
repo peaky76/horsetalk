cf53399a4cb4b8cd2b2f604cbeca923afa3ab8c8
peaky76 <robertjamespeacock@gmail.com>
2023-04-07 22:39:23 +0100

    refactor: RaceTitle._lookup helper method

--- horsetalk/race_title.py
+++ horsetalk/race_title.py
@@ -1,3 +1,5 @@
+from enum import Enum
+from typing import List
 from horsetalk import Obstacle, RaceAgeStatus
 
 
@@ -6,25 +8,17 @@ class RaceTitle:
     def parse(title: str):
         words = title.split()
         return {
-            "age_status": (
-                next(
-                    (
-                        found_value
-                        for word in words
-                        if (found_value := getattr(RaceAgeStatus, word, None))
-                        is not None
-                    ),
-                    None,
-                )
-            ),
-            "obstacle": (
-                next(
-                    (
-                        found_value
-                        for word in words
-                        if (found_value := getattr(Obstacle, word, None)) is not None
-                    ),
-                    None,
-                )
-            ),
+            "age_status": RaceTitle._lookup(words, RaceAgeStatus),
+            "obstacle": RaceTitle._lookup(words, Obstacle),
         }
+
+    @staticmethod
+    def _lookup(words: List[str], enum: Enum):
+        return next(
+            (
+                found_value
+                for word in words
+                if (found_value := getattr(enum, word, None)) is not None
+            ),
+            None,
+        )
