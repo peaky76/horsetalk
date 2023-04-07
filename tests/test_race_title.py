7963065e327103c6f47ca448e9ddf9b8155e216e
peaky76 <robertjamespeacock@gmail.com>
2023-04-07 22:34:11 +0100

    feat: RaceTitle.parse handles age status

--- tests/test_race_title.py
+++ tests/test_race_title.py
@@ -1,4 +1,4 @@
-from horsetalk import Obstacle, RaceTitle
+from horsetalk import Obstacle, RaceAgeStatus, RaceTitle
 
 
 def test_race_title_parse_returns_dict():
@@ -16,4 +16,18 @@ def test_race_title_parse_can_correctly_identify_obstacle_when_present():
 def test_race_title_parse_can_correctly_identify_obstacle_when_not_present():
     expected = None
     actual = RaceTitle.parse("HAPPY NEW YEAR HANDICAP (4)")["obstacle"]
+    assert expected == actual
+
+
+def test_race_title_parse_can_correctly_identify_age_status_when_present():
+    expected = RaceAgeStatus.JUVENILE
+    actual = RaceTitle.parse(
+        "BETSAFE BEST ODDS GUARANTEED JUVENILE MAIDEN HURDLE (Qualifier) (4)"
+    )["age_status"]
+    assert expected == actual
+
+
+def test_race_title_parse_can_correctly_identify_age_status_when_not_present():
+    expected = None
+    actual = RaceTitle.parse("HAPPY NEW YEAR HANDICAP (4)")["age_status"]
     assert expected == actual
