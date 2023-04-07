e3a0d5d3a361454c3c88c0b425b32380206b3193
peaky76 <robertjamespeacock@gmail.com>
2023-04-07 22:34:11 +0100

    feat: RaceTitle.parse handles age status

--- horsetalk/race_title.py
+++ horsetalk/race_title.py
@@ -1,4 +1,5 @@
 from horsetalk.obstacle import Obstacle
+from horsetalk.race_age_status import RaceAgeStatus
 
 
 class RaceTitle:
@@ -6,6 +7,17 @@ class RaceTitle:
     def parse(title: str):
         words = title.split()
         return {
+            "age_status": (
+                next(
+                    (
+                        found_value
+                        for word in words
+                        if (found_value := getattr(RaceAgeStatus, word, None))
+                        is not None
+                    ),
+                    None,
+                )
+            ),
             "obstacle": (
                 next(
                     (
@@ -15,5 +27,5 @@ class RaceTitle:
                     ),
                     None,
                 )
-            )
+            ),
         }
