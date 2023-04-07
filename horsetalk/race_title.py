3185c84bb647194a44a9e4bb6a6a83a601f89d36
peaky76 <robertjamespeacock@gmail.com>
2023-04-07 22:27:45 +0100

    feat: RaceTitle.parse handles obstacle

--- horsetalk/race_title.py
+++ horsetalk/race_title.py
@@ -4,4 +4,16 @@ from horsetalk.obstacle import Obstacle
 class RaceTitle:
     @staticmethod
     def parse(title: str):
-        return {}
+        words = title.split()
+        return {
+            "obstacle": (
+                next(
+                    (
+                        found_value
+                        for word in words
+                        if (found_value := getattr(Obstacle, word, None)) is not None
+                    ),
+                    None,
+                )
+            )
+        }
