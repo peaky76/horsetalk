b08d5798603e6385805c0d6ff3886a454655898d
peaky76 <robertjamespeacock@gmail.com>
2023-04-07 22:27:45 +0100

    feat: RaceTitle.parse handles obstacle

--- tests/test_race_title.py
+++ tests/test_race_title.py
@@ -5,3 +5,15 @@ def test_race_title_parse_returns_dict():
     expected = dict
     actual = type(RaceTitle.parse(""))
     assert expected == actual
+
+
+def test_race_title_parse_can_correctly_identify_obstacle_when_present():
+    expected = Obstacle.HURDLE
+    actual = RaceTitle.parse("HAPPY NEW YEAR NOVICES HURDLE (4)")["obstacle"]
+    assert expected == actual
+
+
+def test_race_title_parse_can_correctly_identify_obstacle_when_not_present():
+    expected = None
+    actual = RaceTitle.parse("HAPPY NEW YEAR HANDICAP (4)")["obstacle"]
+    assert expected == actual
