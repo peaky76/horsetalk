c1e9f164c5952b4b5887dae3f7c4f6a8976f08ff
peaky76 <robertjamespeacock@gmail.com>
2023-04-08 16:14:13 +0100

    refactor: FormFigures.parse list comprehension

--- horsetalk/form_figures.py
+++ horsetalk/form_figures.py
@@ -4,14 +4,11 @@ from horsetalk import Disaster, FinishingPosition, FormBreak
 class FormFigures:
     @staticmethod
     def parse(form_figures: str):
-        retval = []
-
-        for figure in form_figures:
-            if figure.isdigit():
-                retval.append(FinishingPosition(int(figure)))
-            elif figure in [member.value for member in FormBreak]:
-                retval.append(FormBreak(figure))
-            else:
-                retval.append(Disaster[figure])
-
-        return retval
+        return [
+            FinishingPosition(int(figure))
+            if figure.isdigit()
+            else FormBreak(figure)
+            if figure in [member.value for member in FormBreak]
+            else Disaster[figure]
+            for figure in form_figures
+        ]
