29fbe500a8eba03368e8ad73f09860d7cb2dce6b
peaky76 <robertjamespeacock@gmail.com>
2023-04-08 17:47:31 +0100

    test: rename tests correctly

--- tests/test_age_category.py
+++ tests/test_age_category.py
@@ -1,17 +1,17 @@
 from horsetalk import AgeCategory
 
 
-def test_race_age_status_can_be_created_from_enum():
+def test_age_category_can_be_created_from_enum():
     assert AgeCategory.JUVENILE == AgeCategory(1)
 
 
-def test_race_age_status_can_be_created_from_name():
+def test_age_category_can_be_created_from_name():
     assert AgeCategory.JUVENILE == AgeCategory["JUVENILE"]
 
 
-def test_race_age_status_can_be_created_from_lowercase_name():
+def test_age_category_can_be_created_from_lowercase_name():
     assert AgeCategory.JUVENILE == AgeCategory["juvenile"]
 
 
-def test_race_age_status_can_be_created_from_apostrophised_name():
+def test_age_category_can_be_created_from_apostrophised_name():
     assert AgeCategory.VETERAN == AgeCategory["VETERAN'S"]
