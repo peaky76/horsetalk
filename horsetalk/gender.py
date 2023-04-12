a5dcf042a4fead9644c08178b32d37cd34afadbb
peaky76 <robertjamespeacock@gmail.com>
2023-04-12 23:45:29 +0100

    feat: Gender.determine method with sex, official_age args

--- horsetalk/gender.py
+++ horsetalk/gender.py
@@ -41,3 +41,14 @@ class Gender(ParsingEnum):
             raise ValueError("Not enough information to provide sex of horse")
 
         return Sex.FEMALE if self in [Gender.FILLY, Gender.MARE] else Sex.MALE
+
+    @staticmethod
+    def determine(sex: Sex, official_age: int):
+        if official_age == 0:
+            return Gender.FOAL
+        if official_age == 1:
+            return Gender.YEARLING
+        if official_age <= 3:
+            return Gender.COLT if sex is sex.MALE else Gender.FILLY
+        else:
+            return Gender.STALLION if sex is sex.MALE else Gender.MARE
