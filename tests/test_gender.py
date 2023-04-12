95030047e82eb636acfe90b7d90585e95aa326bb
peaky76 <robertjamespeacock@gmail.com>
2023-04-12 23:36:09 +0100

    feat: Gender.sex property

--- tests/test_gender.py
+++ tests/test_gender.py
@@ -1,4 +1,5 @@
-from horsetalk.gender import Gender
+import pytest
+from horsetalk import Gender, Sex
 
 
 def test_gender_can_be_created_from_enum():
@@ -15,3 +16,16 @@ def test_gender_can_be_created_from_lowercase_name():
 
 def test_gender_can_be_created_from_abbreviation():
     assert Gender.COLT == Gender["C"]
+
+
+def test_gender_sex_returns_male_for_male_genders():
+    assert Sex.MALE == Gender["COLT"].sex
+
+
+def test_gender_sex_returns_female_for_female_genders():
+    assert Sex.FEMALE == Gender["FILLY"].sex
+
+
+def test_gender_sex_raises_error_if_not_enough_info():
+    with pytest.raises(ValueError):
+        Gender["YEARLING"].sex
