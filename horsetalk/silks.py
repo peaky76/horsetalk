0e6a05f5d93985f2a5bd99dbf84ba17e3d5798b6
peaky76 <robertjamespeacock@gmail.com>
2023-05-21 19:23:15 +0100

    feat: Silks.Shape enum

--- horsetalk/silks.py
+++ horsetalk/silks.py
@@ -1,4 +1,38 @@
+from enum import auto
+from horsetalk.parsing_enum import ParsingEnum
+
+
 class Silks:
+    class Shape(ParsingEnum):
+        """An enumeration of shapes that can appear on jockey silks."""
+
+        ARMLET = auto()
+        BRACES = auto()
+        CHECK = auto()
+        CHEVRON = auto()
+        CHEVRONS = auto()
+        CROSS_OF_LORRAINE = auto()
+        CROSS_SASHES = auto()
+        CUFFS = auto()
+        DIAMOND = auto()
+        DIABOLO = auto()
+        DISC = auto()
+        EPAULETS = auto()
+        HALVED = auto()
+        HOOP = auto()
+        HOOPS = auto()
+        INVERTED_TRIANGLE = auto()
+        PANEL = auto()
+        QUARTERED = auto()
+        SASH = auto()
+        SEAMS = auto()
+        SPOTS = auto()
+        STAR = auto()
+        STARS = auto()
+        STRIPE = auto()
+        STRIPED = auto()
+        TRIPLE_DIAMOND = auto()
+
     @classmethod
     def parse(cls, description: str) -> dict:
         """Parses a description of silks into component parts.
@@ -9,4 +43,4 @@ class Silks:
         self = cls()
         self.description = description
 
-        return {}
+        return {"shirt": None, "sleeves": None, "cap": None}
--- tests/test_silks.py
+++ tests/test_silks.py
@@ -3,6 +3,18 @@ from horsetalk import (
 )
 
 
+def test_silks_shape_can_be_created_from_name():
+    assert Silks.Shape.STAR == Silks.Shape["STAR"]
+
+
+def test_headgear_can_be_created_from_lowercase_name():
+    assert Silks.Shape.STAR == Silks.Shape["star"]
+
+
+def test_headgear_can_be_created_from_spaced_name():
+    assert Silks.Shape.INVERTED_TRIANGLE == Silks.Shape["Inverted triangle"]
+
+
 def test_silks_parse_returns_dict():
     expected = dict
     actual = type(Silks.parse("orange and blue hoops, white sleeves, orange cap"))
