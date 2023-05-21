e1120bbebdec4e348418a87b252bac726bfd1f21
peaky76 <robertjamespeacock@gmail.com>
2023-05-21 19:57:09 +0100

    refactor: rename Silks.Shape -> Silks.Pattern

--- tests/test_silks.py
+++ tests/test_silks.py
@@ -15,16 +15,16 @@ def test_silks_colour_can_be_created_from_spaced_name():
     assert Silks.Colour.DARK_BLUE == Silks.Colour["dark blue"]
 
 
-def test_silks_shape_can_be_created_from_name():
-    assert Silks.Shape.STAR == Silks.Shape["STAR"]
+def test_silks_pattern_can_be_created_from_name():
+    assert Silks.Pattern.STAR == Silks.Pattern["STAR"]
 
 
-def test_silks_shape_can_be_created_from_lowercase_name():
-    assert Silks.Shape.STAR == Silks.Shape["star"]
+def test_silks_pattern_can_be_created_from_lowercase_name():
+    assert Silks.Pattern.STAR == Silks.Pattern["star"]
 
 
-def test_silks_shape_can_be_created_from_spaced_name():
-    assert Silks.Shape.INVERTED_TRIANGLE == Silks.Shape["Inverted triangle"]
+def test_silks_pattern_can_be_created_from_spaced_name():
+    assert Silks.Pattern.INVERTED_TRIANGLE == Silks.Pattern["Inverted triangle"]
 
 
 def test_silks_parse_returns_dict():
