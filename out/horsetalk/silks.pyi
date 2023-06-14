1c76d07802e6836ab7e5b108a71e7eeda27ced18
peaky76 <robertjamespeacock@gmail.com>
2023-06-15 00:14:36 +0100

    fix: type stubs and formatting

--- out/horsetalk/silks.pyi
+++ out/horsetalk/silks.pyi
@@ -1,7 +1,10 @@
 from _typeshed import Incomplete
 from horsetalk.parsing_enum import ParsingEnum as ParsingEnum
+from typing import Optional
 
 class Silks:
+    description: str
+
     class Colour(ParsingEnum):
         BEIGE: int
         BLACK: int
@@ -24,6 +27,7 @@ class Silks:
         YELLOW: int
         @staticmethod
         def phrases(): ...
+
     class Pattern(ParsingEnum):
         ARMLETS: int
         ARMLET = ARMLETS
@@ -64,13 +68,19 @@ class Silks:
         def phrases(): ...
         @classmethod
         def body_only(cls): ...
+
     class Element:
         primary: Incomplete
         secondary: Incomplete
         pattern: Incomplete
-        def __init__(self, primary: Silks.Colour, secondary: Silks.Colour = ..., pattern: Silks.Shape = ...) -> None: ...
+        def __init__(
+            self,
+            primary: Silks.Colour,
+            secondary: Optional["Silks.Colour"] = ...,
+            pattern: Optional["Silks.Pattern"] = ...,
+        ) -> None: ...
         def __eq__(self, other): ...
-    description: Incomplete
+
     @classmethod
     def parse(cls, description: str) -> Silks: ...
     @property
