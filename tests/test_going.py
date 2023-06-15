54487dbf27b76ebf34be3a12c5e089f47a8f62e1
peaky76 <robertjamespeacock@gmail.com>
2023-06-15 21:16:57 +0100

    feat: Going class

---
+++ tests/test_going.py
@@ -0,0 +1,59 @@
+from horsetalk import AWGoingDescription, Going, TurfGoingDescription
+
+
+def test_going_can_be_initialized_with_string():
+    assert Going("Good")
+
+
+def test_going_can_be_initialized_with_string_and_float():
+    assert Going("Good", 7.0)
+
+
+def test_going_init_sets_description():
+    assert "Good" == Going("Good").description
+
+
+def test_going_init_sets_reading_on_default():
+    assert None is Going("Good").reading
+
+
+def test_going_init_sets_reading_when_given():
+    assert 7.0 == Going("Good", 7.0).reading
+
+
+def test_going_init_sets_primary_property_with_enum_for_turf_going():
+    assert TurfGoingDescription.GOOD == Going("Good").primary
+
+
+def test_going_init_sets_primary_property_with_enum_for_all_weather_going():
+    assert AWGoingDescription.STANDARD_TO_SLOW == Going("Standard to slow").primary
+
+
+def test_going_init_sets_primary_property_with_enum_when_secondary_given():
+    assert TurfGoingDescription.GOOD == Going("Good, Good to Soft in places").primary
+
+
+def test_going_init_sets_secondary_property_with_enum_for_turf_going():
+    assert (
+        TurfGoingDescription.GOOD_TO_SOFT
+        == Going("Good, Good to Soft in places").secondary
+    )
+
+
+def test_going_init_sets_secondary_property_with_enum_for_all_weather_going():
+    assert (
+        AWGoingDescription.STANDARD_TO_SLOW
+        == Going("Standard, Standard to slow in places").secondary
+    )
+
+
+def test_going_init_does_not_set_secondary_property_when_only_primary_given():
+    assert None is Going("Good").secondary
+
+
+def test_going_value_returns_primary_value_when_only_primary_given():
+    assert 8 == Going("Good").value
+
+
+def test_going_value_returns_mean_of_primary_and_secondary_values_when_both_given():
+    assert 7.5 == Going("Good, Good to Soft in places").value
