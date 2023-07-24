a04a81172e51979eb1a97fe8e4bf083ff076108c
peaky76 <robertjamespeacock@gmail.com>
2023-07-24 23:17:14 +0100

    test: fix method call in RaceConditions test

--- tests/test_race_conditions.py
+++ tests/test_race_conditions.py
@@ -4,7 +4,7 @@ from horsetalk import Going, RaceConditions, RaceDistance, Surface
 
 def test_race_conditions_can_be_initialized_with_date_distance_going_surface():
     assert RaceConditions(
-        date=pendulum.parse("2023-06-01 14:00"),
+        datetime=pendulum.parse("2023-06-01 14:00"),
         distance=RaceDistance("5f"),
         going=Going("Good"),
         surface=Surface.TURF,
