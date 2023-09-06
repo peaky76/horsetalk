dd6af61795c5a9a7f1c4b348c4a518807b955b8f
peaky76 <robertjamespeacock@gmail.com>
2023-09-06 21:20:44 +0100

    docs: docstrings for racecourse enums

--- horsetalk/racecourse_contour.py
+++ horsetalk/racecourse_contour.py
@@ -2,6 +2,11 @@ from .parsing_enum import ParsingEnum
 
 
 class RacecourseContour(ParsingEnum):
+    """
+    An enumeration representing the contour of a racecourse.
+
+    """
+
     UNKNOWN = 0
     FLAT = 1
     DOWNHILL = 2
