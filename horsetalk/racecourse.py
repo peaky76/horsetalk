a2c70c0663c3b41dc06a3d7e2bd2c2d9d03fd502
peaky76 <robertjamespeacock@gmail.com>
2023-09-06 21:51:05 +0100

    docs: docstrings for Racecourse

--- horsetalk/racecourse.py
+++ horsetalk/racecourse.py
@@ -6,7 +6,24 @@ from .surface import Surface
 
 
 class Racecourse:
+    """
+    A class for grouping together racecourse characteristics into a single object.
+
+    """
+
     def __init__(self, name: str, surface: Surface, **kwargs):
+        """
+        Initialize a Racecourse instance.
+
+        Args:
+            name: The name of the racecourse
+            surface: The surface on which the racecourse is run
+            handedness: The handedness of the racecourse
+            contour: The contour of the racecourse
+            shape: The shape of the racecourse
+            style: The style of the racecourse
+
+        """
         self.name = name
         self.surface = surface
         self.handedness = Handedness[kwargs.get("handedness", "unknown")]
