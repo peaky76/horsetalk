43706a0954dc36f68b39bb374b423fcac852b4f0
peaky76 <robertjamespeacock@gmail.com>
2023-09-06 21:37:47 +0100

    feat: add Surface to Racecourse.__init__

--- horsetalk/racecourse.py
+++ horsetalk/racecourse.py
@@ -2,11 +2,13 @@ from .handedness import Handedness
 from .racecourse_contour import RacecourseContour
 from .racecourse_shape import RacecourseShape
 from .racecourse_style import RacecourseStyle
+from .surface import Surface
 
 
 class Racecourse:
-    def __init__(self, name: str, **kwargs):
+    def __init__(self, name: str, surface: Surface, **kwargs):
         self.name = name
+        self.surface = surface
         self.handedness = Handedness[kwargs.get("handedness", "unknown")]
         self.contour = RacecourseContour[kwargs.get("contour", "unknown")]
         self.shape = RacecourseShape[kwargs.get("shape", "unknown")]
