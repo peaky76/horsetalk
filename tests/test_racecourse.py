5b0719bb4152a43d75726c22077f171e54dd04b3
peaky76 <robertjamespeacock@gmail.com>
2023-09-06 21:37:47 +0100

    feat: add Surface to Racecourse.__init__

--- tests/test_racecourse.py
+++ tests/test_racecourse.py
@@ -5,51 +5,68 @@ from horsetalk import (
     RacecourseContour,
     RacecourseShape,
     RacecourseStyle,
+    Surface,
 )
 
 
 def test_racecourse_can_be_initialized_without_characteristics():
-    assert Racecourse("Portman Park")
+    assert Racecourse("Portman Park", Surface.TURF)
 
 
 def test_racecourse_default_handedness():
-    assert Racecourse("Portman Park").handedness == Handedness.UNKNOWN
+    assert Racecourse("Portman Park", Surface.TURF).handedness == Handedness.UNKNOWN
 
 
 def test_racecourse_default_contour():
-    assert Racecourse("Portman Park").contour == RacecourseContour.UNKNOWN
+    assert Racecourse("Portman Park", Surface.TURF).contour == RacecourseContour.UNKNOWN
 
 
 def test_racecourse_default_shape():
-    assert Racecourse("Portman Park").shape == RacecourseShape.UNKNOWN
+    assert Racecourse("Portman Park", Surface.TURF).shape == RacecourseShape.UNKNOWN
 
 
 def test_racecourse_default_style():
-    assert Racecourse("Portman Park").style == RacecourseStyle.UNKNOWN
+    assert Racecourse("Portman Park", Surface.TURF).style == RacecourseStyle.UNKNOWN
 
 
 def test_racecourse_can_be_initialized_with_params():
     assert Racecourse(
-        "Aintree", handedness="left", contour="flat", shape="oval", style="galloping"
+        "Aintree",
+        Surface.TURF,
+        handedness="left",
+        contour="flat",
+        shape="oval",
+        style="galloping",
     )
 
 
 def test_racecourse_handedness():
-    assert Racecourse("Aintree", handedness="left").handedness == Handedness.LEFT
+    assert (
+        Racecourse("Aintree", Surface.TURF, handedness="left").handedness
+        == Handedness.LEFT
+    )
 
 
 def test_racecourse_contour():
-    assert Racecourse("Aintree", contour="flat").contour == RacecourseContour.FLAT
+    assert (
+        Racecourse("Aintree", Surface.TURF, contour="flat").contour
+        == RacecourseContour.FLAT
+    )
 
 
 def test_racecourse_shape():
-    assert Racecourse("Aintree", shape="oval").shape == RacecourseShape.OVAL
+    assert (
+        Racecourse("Aintree", Surface.TURF, shape="oval").shape == RacecourseShape.OVAL
+    )
 
 
 def test_racecourse_style():
-    assert Racecourse("Aintree", style="galloping").style == RacecourseStyle.GALLOPING
+    assert (
+        Racecourse("Aintree", Surface.TURF, style="galloping").style
+        == RacecourseStyle.GALLOPING
+    )
 
 
 def test_racecourse_init_raises_error_if_characteristic_is_invalid():
     with pytest.raises(KeyError):
-        Racecourse("Aintree", handedness="invalid")
+        Racecourse("Aintree", Surface.TURF, handedness="invalid")
