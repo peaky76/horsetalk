43c43e12c89fb49fbddd3f3369621e1d59823dd0
peaky76 <robertjamespeacock@gmail.com>
2023-09-15 22:45:47 +0100

    feat: RaceClass class

---
+++ tests/test_race_class.py
@@ -0,0 +1,31 @@
+import pytest
+from horsetalk import RaceClass
+
+
+def test_race_class_init_possible_with_valid_int():
+    assert RaceClass(1)
+
+
+def test_race_class_init_possible_with_valid_str():
+    assert RaceClass("1")
+
+
+def test_race_class_init_not_possible_with_invalid_int():
+    with pytest.raises(ValueError):
+        RaceClass(0)
+
+
+def test_race_class_str():
+    assert str(RaceClass(1)) == "Class 1"
+
+
+def test_race_class_eq():
+    assert RaceClass(1) == RaceClass(1)
+
+
+def test_race_class_gt():
+    assert RaceClass(1) > RaceClass(2)
+
+
+def test_race_class_lt():
+    assert RaceClass(2) < RaceClass(1)
