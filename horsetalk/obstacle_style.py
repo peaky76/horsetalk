5a04b0ff3299fcab9c6451ea00332294c5ca1098
peaky76 <robertjamespeacock@gmail.com>
2023-09-17 18:33:14 +0100

    build: import ParsingEnum from peak_utility

--- horsetalk/obstacle_style.py
+++ horsetalk/obstacle_style.py
@@ -1,4 +1,4 @@
-from .parsing_enum import ParsingEnum
+from peak_utility.enumeration.parsing_enum import ParsingEnum
 
 
 class ObstacleStyle(ParsingEnum):
--- out/horsetalk/going_description.pyi
+++ out/horsetalk/going_description.pyi
@@ -1,3 +1,3 @@
-from .parsing_enum import ParsingEnum as ParsingEnum
+from peak_utility.enumeration.parsing_enum import ParsingEnum as ParsingEnum
 
 class GoingDescription(ParsingEnum): ...
