6fcd8c2641b81b90ee3544b263b67ca0972bae6b
peaky76 <robertjamespeacock@gmail.com>
2023-09-23 18:46:43 +0100

    refactor: remove unneeded arithmentic methods from Draw

--- horsetalk/draw.py
+++ horsetalk/draw.py
@@ -14,15 +14,3 @@ class Draw(RepresentationalInt):
             raise ValueError("Draw must represent an integer value")
 
         return super().__new__(cls, value)
-
-    def __add__(self, other: Self) -> bool:
-        raise TypeError("Instances of draw cannot be added")
-
-    def __sub__(self, other: Self) -> bool:
-        raise TypeError("Instances of draw cannot be subtracted")
-
-    def __mul__(self, other: Self) -> bool:
-        raise TypeError("Instances of draw cannot be multiplied")
-
-    def __truediv__(self, other: Self) -> bool:
-        raise TypeError("Instances of draw cannot be divided")
