1897a010f3a4169c9d3e1bda1321a0ce739cd438
peaky76 <robertjamespeacock@gmail.com>
2023-09-23 18:47:11 +0100

    refactor: remove unneeded __new__ from Draw

--- horsetalk/draw.py
+++ horsetalk/draw.py
@@ -3,14 +3,4 @@ from peak_utility.number import RepresentationalInt
 
 
 class Draw(RepresentationalInt):
-    def __new__(cls, value: int | str) -> None:
-        """
-        Create a Draw instance.
-
-        Args:
-            value: The stall number in which the horse is drawn.
-        """
-        if not str(int(value)) == str(value):
-            raise ValueError("Draw must represent an integer value")
-
-        return super().__new__(cls, value)
+    pass
