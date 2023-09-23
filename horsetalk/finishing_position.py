076df8e0fa47051dea12206580c59ac8af83c057
peaky76 <robertjamespeacock@gmail.com>
2023-09-23 18:21:54 +0100

    refactor: remove unneeded comparators in FinishingPosition

--- horsetalk/finishing_position.py
+++ horsetalk/finishing_position.py
@@ -16,9 +16,6 @@ class FinishingPosition(Ordinal):
 
         return super().__new__(cls, value)
 
-    def __bool__(self):
-        return int(self) >= 0
-
     def __repr__(self):
         if int(self) == 0:
             return "<FinishingPosition: Unplaced>"
@@ -28,18 +25,3 @@ class FinishingPosition(Ordinal):
         if int(self) == 0:
             return "Unplaced"
         return super().__repr__()
-
-    def __eq__(self, other: object) -> bool:
-        return isinstance(other, (int, FinishingPosition)) and int(self) == int(other)
-
-    def __lt__(self, other: int) -> bool:
-        return int(self) > int(other)
-
-    def __le__(self, other: int) -> bool:
-        return int(self) >= int(other)
-
-    def __gt__(self, other: int) -> bool:
-        return int(self) < int(other)
-
-    def __ge__(self, other: int) -> bool:
-        return int(self) <= int(other)
