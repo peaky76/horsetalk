29adbcf26e25e2412a7e0cae10d63d192f4fc5b8
peaky76 <robertjamespeacock@gmail.com>
2023-10-07 23:59:34 +0100

    feat: AgeRestriction.__repr__ and __str__

--- tests/test_age_restriction.py
+++ tests/test_age_restriction.py
@@ -17,17 +17,41 @@ def test_age_restriction_maximum_when_open_ended_restriction():
     assert AgeRestriction("3yo+").maximum == None
 
 
-def test_age_restriction_minimum_when_age_limited_restriction():
+def test_age_restriction_minimum_when_upper_bounded_restriction():
     assert AgeRestriction("2-4yo").minimum == 2
 
 
-def test_age_restrictions_maximum_when_age_limited_restriction():
+def test_age_restrictions_maximum_when_upper_bounded_restriction():
     assert AgeRestriction("2-4yo").maximum == 4
 
 
-def test_age_restrictions_minimum_when_longhand():
+def test_age_restriction_minimum_when_longhand():
     assert AgeRestriction("2 year olds only").minimum == 2
 
 
-def test_age_restrictions_maximum_when_longhand():
+def test_age_restriction_maximum_when_longhand():
     assert AgeRestriction("2 year olds only").maximum == 2
+
+
+def test_age_restriction_repr_when_single_year_restriction():
+    assert repr(AgeRestriction("2yo")) == "<AgeRestriction: 2yo>"
+
+
+def test_age_restriction_repr_when_open_ended_restriction():
+    assert repr(AgeRestriction("3yo+")) == "<AgeRestriction: 3yo+>"
+
+
+def test_age_restriction_repr_when_upper_bounded_restriction():
+    assert repr(AgeRestriction("2-4yo")) == "<AgeRestriction: 2-4yo>"
+
+
+def test_age_restriction_str_when_single_year_restriction():
+    assert str(AgeRestriction("2yo")) == "2yo"
+
+
+def test_age_restriction_str_when_open_ended_restriction():
+    assert str(AgeRestriction("3yo+")) == "3yo+"
+
+
+def test_age_restriction_str_when_upper_bounded_restriction():
+    assert str(AgeRestriction("2-4yo")) == "2-4yo"
