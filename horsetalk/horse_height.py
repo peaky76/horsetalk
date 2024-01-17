228ce34c22cd4bfb1d0fb08b187a44ca7526d950
peaky76 <robertjamespeacock@gmail.com>
2024-01-17 22:32:59 +0000

    refactor: replace measurement with pint in HorseHeight

--- horsetalk/horse_height.py
+++ horsetalk/horse_height.py
@@ -1,52 +1,47 @@
-import re
 from decimal import Decimal
 
-from measurement.measures import Distance  # type: ignore
+from horsetalk._pint import Q_
 
 
-class HorseHeight(Distance):
+class HorseHeight(Q_):
     """
-    A class for measuring a horse's height, created by wrapping the measurement library Distance class to allow
-    for the use of hands as a unit.
+    A class for measuring a horse's height.
 
     """
 
-    def __init__(self, distance: str) -> None:
+    def __new__(cls, *args, **kwargs):
         """
-        Initialize a HorseHeight object from a string.
+        Initializes a HorseHeight object.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            A HorseHeight object.
         """
-        pattern = re.compile(r"(\d+\D+)")
-        vals_and_units = pattern.findall(distance.replace(" ", ""))
-
-        height = Distance(inch=0)
-        for vu in vals_and_units:
-            matches = re.compile(r"(\d+)(\D+)").match(vu)
-            if matches:
-                val, unit = matches.groups()
-                if unit == "hands" or unit == "hh":
-                    height += Distance(inch=int(val) * 4)
-                else:
-                    height += Distance(**{unit: int(val)})
-
-        super().__init__(self, inch=height.inch)
-
+        
+        instance = Q_.__new__(Q_, *args, **kwargs)
+        instance.__class__ = cls
+        return instance
+        
     def __repr__(self) -> str:
         """
         Returns:
             A representation of the HorseHeight object.
         """
-        return f"<HorseHeight: {self.hand}hh>"
+        return f"<HorseHeight: {self!s}>"
 
     def __str__(self) -> str:
         """
         Returns:
             A string representation of the HorseHeight object.
         """
-        return f"{self.hand}hh"
+        return f"{self.to('hh').magnitude}hh"
 
     @property
     def hand(self) -> Decimal:
         """
         Returns the height in hands.
         """
-        return Decimal(self.inch / 4)
+        return self.to("hh").magnitude
