0d2888fa3c044ac6a55feab0332ecc0a79be0fc0
peaky76 <robertjamespeacock@gmail.com>
2024-01-18 00:47:52 +0000

    refactor: override furlong unit definition

--- horsetalk/_pint.py
+++ horsetalk/_pint.py
@@ -2,6 +2,6 @@ from pint import UnitRegistry
 
 ureg = UnitRegistry()
 ureg.define('hand = 4 * inch = hh')
-ureg.define('@alias furlong = f')
+ureg.define('furlong = 0.125 * mile = f')
 ureg.define('@alias yard = y')
 Q_ = ureg.Quantity
