7c0df0e5af9c6003bd123d47c7cb8ab3a8c864af
peaky76 <robertjamespeacock@gmail.com>
2024-01-18 17:41:44 +0000

    refactor: use en_GB in HorsetalkQuantity

--- horsetalk/quantity.py
+++ horsetalk/quantity.py
@@ -2,7 +2,7 @@ import re
 
 from pint import UnitRegistry
 
-ureg = UnitRegistry()
+ureg = UnitRegistry(fmt_locale="en_GB")
 ureg.define("hand = 4 * inch = hh")
 ureg.define("furlong = 0.125 * mile = f")
 ureg.define("@alias yard = y")
